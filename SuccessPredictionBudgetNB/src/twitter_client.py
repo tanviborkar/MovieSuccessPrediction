d174b14d317007121873532a5289d7dfab7017c4
manjusha9 <ray.manjusha@gmail.com>
2017-04-22 23:55:40 -0400

    Updated twitter client

--- SuccessPredictionBudgetNB/src/twitter_client.py
+++ SuccessPredictionBudgetNB/src/twitter_client.py
@@ -10,10 +10,10 @@ def get_twitter_auth():
     Return: tweepy.OAuthHandler object
     """
     try:
-        consumer_key = "DYLog0JNQCt3DIq7AECGDuUzE"
-        consumer_secret = "UFzGJRFaFbOjViUAFsX2VluWqChUBGomcwh0PW7kUMjdoyBafC"
-        access_token = "838142549365784580-ZaZ6WCWwRENH4LiKz6mkrS6stSiTUsf"
-        access_secret = "DTtWJJIk8cYRiL3pHSS2xm8ixd22SoJudmFUWrZ30c54s"
+        consumer_key = ""
+        consumer_secret = ""
+        access_token = ""
+        access_secret = ""
     except KeyError:
         sys.stderr.write("TWITTER_* environment variables not set\n")
         sys.exit(1)    
